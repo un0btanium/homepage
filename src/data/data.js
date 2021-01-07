e8ac3df603efcbfbb3ca32a6c14ba2ed8f804a7b
un0btanium <unobtaniumLoL@gmail.com>
2021-01-07 20:11:25 +0100

    Add more projects and text

--- src/data/data.js
+++ src/data/data.js
@@ -3,7 +3,7 @@ var data = {
 		{
 			title: "Star Citizen - Road to Dynamic Server Meshing",
 			date: "June 2020",
-			info: "In-depth Prezi Presentation about the Architecture of the Dynamic Server Meshing software system",
+			info: "Prezi Presentation - Overview of the Architecture of the Dynamic Server Meshing software system",
 			text: "I have been very excited about the technology that one day will be <b>Star Citizens Dynamic Server Meshing</b>. However, I had trouble finding any great single source of information which gives a good overview and explanation of what it is and how CIG is planning to realize it and what the status currently is. Therefore, I took it upon myself, <b>researched all the information available</b> to us and created <b>a visual and textual explanation as an extensive Prezi presentation with over 100 individual slides</b> for everyone to check out and refer to. All the available information I was able to find can be found in the presentation making it a great source about Dynamic Server Meshing! The sources used were provided at the end of the presentation as well.",
 			links: [
 				{
@@ -31,9 +31,19 @@ var data = {
 			title: "CodingBuddy",
 			date: "2019-2020",
 			info: "Java, JavaParser, Node.js, Express, MongoDB, React",
-			text: "This website <b>visualizes the execution of Java programs</b>. It allows programming students to write Java code and then provides them with <b>step-by-step statement highlighting</b> at runtime. The goal was to help programming novices get a much more <b>intuitive understanding about how the computer executes their code</b>. This project was made possible by creating a <b>custom debugger</b> by parsing the users Java program and <b>injecting custom code</b> at various Abstract Syntax Tree nodes. At runtime, that code calls custom functions which <b>log the execution of each statement</b>. The position information of statements from the parser was used to be able to correctly highlight the executed statement on the frontend editor. This project is <b>currently expanded upon</b> to optionally display the Stack and Heap of the Java Virtual Machine through the Java Platform Debugger Architecture (JPDA). This information could be useful for visualizing more advanced programming concepts.",
+			text: "This website <b>visualizes the execution of Java programs</b>. It allows programming students to write Java code and then provides them with <b>step-by-step statement highlighting</b> at runtime. The goal was to help programming novices to develop a much more <b>intuitive understanding about how the computer executes their code</b>. This project was made possible by creating a <b>custom debugger</b> by parsing the users Java program and <b>injecting custom code</b> at various Abstract Syntax Tree nodes. At runtime, that code calls custom functions which <b>log the execution of each statement</b>. The position information of statements from the parser was used to be able to correctly highlight the executed statement in the frontend editor. This project is <b>currently expanded upon</b> to optionally display the Stack and Heap of the Java Virtual Machine through the Java Platform Debugger Architecture (JPDA). This information could be useful for visualizing more advanced programming concepts.",
 			images: [
 				"coding-buddy/codingbuddy_forloop.gif",
+			],
+			links: [
+				{
+					website: "github",
+					url: "https://github.com/un0btanium/Coding-Website",
+				},
+				{
+					website: "github",
+					url: "https://github.com/un0btanium/Code-Simulator"
+				}
 			],
 			isHighlight: true
 		},
@@ -62,7 +72,7 @@ var data = {
 			title: "Phasmophobia Cheatsheet",
 			date: "October 2019",
 			info: "JavaScript, React",
-			text: "TODO",
+			text: "An <b>interactive cheatsheet</b> website for the videogame Phasmophobia. In the game the players hunts ghosts by figuring out what type of ghost they are dealing with. They can narrow down their search through various paranormal activities like freezing temperatures, ghost writing, spirit box talking, etc. This website provides an simple overview over all ghost evidence and lets mark and rule out evidence to narrow down their search more easily.",
 			links: [
 				{
 					website: "github",
@@ -82,7 +92,7 @@ var data = {
 			title: "Teamfight Tactics Cheatsheet",
 			date: "August 2019",
 			info: "JavaScript, React",
-			text: "TODO",
+			text: "An <b>interactive cheatsheet</b> for the League of Legends Teamfight Tactics gamemode. It allows the player to keep track of their items and champions by adding/removing them to then provides them with an overview of the options that they have available.",
 			links: [
 				{
 					website: "github",
@@ -117,10 +127,25 @@ var data = {
 			isHighlight: true
 		},
 		{
+			title: "Static Code Analyzer for University e-Learning Platform",
+			date: "2018-2019",
+			info: "Java, JavaParser",
+			text: "As part of my bachelor thesis I <b>extended the e-learning platform</b> at my university with the functionality of a <b>static code analyzer</b>. The platform was used to organize programming course exercises by allowing <b>students to submit their programs and receive instant feedback</b> through automatic testing. It already supported dynamic testing via JUnit tests at program runtime and was then extended to support static testing of the source code as well to be able to provide additional feedback on code style and quality. The static testing library was designed to be easy to expand and allow the user to individual set up tests for various programming exercises. Statistics had been created to see if there was any correlation between the students code quality and their code functionality which was not the case.",
+			images: [
+				"static-code-analyzer/image1.png",
+				"static-code-analyzer/image2.png",
+				"static-code-analyzer/image3.png",
+				"static-code-analyzer/image4.png",
+				"static-code-analyzer/image5.png",
+				"static-code-analyzer/image6.png"
+			],
+			isHighlight: true
+		},
+		{
 			title: "Factorio Script",
 			date: "July 2017",
 			info: "Java, Antlr4, Factorio BASE-64 Import String",
-			text: "A <b>programming language</b> that allows the user to code in a high level programming language that is then converted into Factorio combinator logic. The compiled result was <b>exported as a base64 Factorio import string</b> which allowed users to compile their programs and then easily import the logic into the game.",
+			text: "A <b>programming language</b> that allows the user to code in a high level programming language that is then converted into Factorio combinator logic. The compiled result was <b>exported as a base64 Factorio blueprint import string</b> which allowed users to compile their programs and then directly import the logic into their game.",
 			links: [
 				{
 					website: "github",
@@ -141,17 +166,28 @@ var data = {
 			],
 		},
 		{
+			title: "Genetic Algorithms",
+			date: "Juni 2017",
+			info: "Java",
+			text: "Having a look at genetic algorithms for various path finding problems like the traveling salesman problem. ",
+			images: [
+				"genetic-algorithms/image1.png",
+				"genetic-algorithms/image2.png",
+				"genetic-algorithms/image3.png"
+			],
+		},
+		{
 			title: "Scalable Shulker Storage System",
 			date: "November 2016",
 			info: "Minecraft Redstone",
-			text: "This creation in Minecraft allowed players to store large amounts of items in Shulker Boxes. The user was able to insert Shulker Boxes into the system and then retrieve them again by querying. The main feature was <b>high query speed</b> which was accomplished <b>by running multiple search engines in parallel</b> (16 are shown in the video). For this to work the Shulker Boxes had to be <b>evenly distributed</b> among all search engines. When Shulker Boxes were taken out of a search engine the system remembered from which engine it was taken out to later correctly insert it back in to always ensure even distribution over multiple queries as well. The query filter item for the search engines was delivered and retrieved via a storage minecart. The YouTube video showcasing the contraption <b>attracted 3.5k+ views and a lot of positive and impressed feedback</b> from the viewers.",
+			text: "This creation in Minecraft allowed players to store large amounts of items in Shulker Boxes. The user was able to insert Shulker Boxes into the system and then retrieve them again by querying. The main feature was <b>high query speed</b> which was accomplished <b>by running multiple search engines in parallel</b> (16 are shown in the video). For this to work the Shulker Boxes had to be <b>evenly distributed</b> among all search engines. When Shulker Boxes were taken out of a search engine the system remembered from which engine it was taken out to later correctly insert it back in to always ensure even distribution over multiple queries as well. The query filter item for the search engines was delivered and retrieved via a storage minecart. The YouTube video showcasing the contraption <b>attracted 3,500+ views and a lot of positive and impressed feedback</b> from the viewers.",
 			video: "7wc2c2ovZ-c"
 		},
 		{
 			title: "Shulker Storage System",
 			date: "October 2016",
 			info: "Minecraft Redstone",
-			text: "This was a first <b>prototype</b> for a Shulker Box storage system in Minecraft which would allow players to <b>store large amounts of items in Shulker Boxes</b> and retrieve them by querying for them. This prototype was designed in a way that would allow it to be <b>scalable later on</b> to have multiple search engines work in parallel to speed up the query process.",
+			text: "This was a first <b>prototype</b> for a Shulker Box storage system in Minecraft which would allow players to <b>store large amounts of items in Shulker Boxes</b> and retrieve them by querying for them. This prototype was designed in a way that would allow it to be <b>scalable later on</b> to have multiple search engines work in parallel to speed up the query process. The video featured a step-by-step tutorial and was able to <b>attract 5,000+ views</b> and a lot of positive feedback.",
 			video: "K4iBWZcXoyA"
 		},
 		{
@@ -196,11 +232,34 @@ var data = {
 			],
 			isHighlight: true
 		},
+
+		{
+			title: "Source Engine Mapping",
+			date: "2015-2018",
+			info: "Source Engine and Source Editor",
+			text: "Every once in a while I created a few maps for the Valve Source Engine via the engines Hammer editor for the popular TTT Gmod mod. I created maps with <b>various themes</b> from Minecraft over abstract to a \"Das verrückte Labyrinth\" inspired map that constantly shifts and changes while playing. In total the handful of maps that I created over the years were <b>downloaded over 150,000+ times</b> from the Steam workshop. It introduced me to the workflow of creating maps for a videogame engine and introduced me to various concepts of optimization in level design.",
+			links: [
+				{
+					website: "steam",
+					url: "https://steamcommunity.com/id/un0btanium/myworkshopfiles/?appid=4000"
+				}
+			],
+			images: [
+				"ttt-mapping/image1.jpg",
+				"ttt-mapping/image2.jpg",
+				"ttt-mapping/image3.png",
+				"ttt-mapping/image4.png",
+				"ttt-mapping/image5.png",
+				"ttt-mapping/image6.png",
+				"ttt-mapping/image7.jpg",
+			]
+		},
+
 		{
 			title: "ComputerCraft Programs",
 			date: "December 2012 - February 2014",
-			info: "LUA",
-			text: "The ComputerCraft mod for the videogame Minecraft introduced programmable computers and robots (called turtles) to the game. In 2013 I was very active on the official ComputerCraft forums <b>creating various programs with the mod</b>. The two most successful ones were the 'Client for OpenPeripheral's Glasses' and 'Ultimate Wood Chopper' programs, both of which <b>featured session persistence</b> by saving and loading their state. The programs were <b>downloaded over 12,000+ times</b> from pastebin.<br/>The linked video was made by SeleckPlays on YT who approached me and asked if he was allowed to create a tutorial on the Ultimate Wood Chooper program I created. At the time this program was highly efficient when it came to producing wood and bio-fuel thanks to the Fir Wood tree not checking for any leaves being in the way and instantly grew which allowed the turtle to non-stop chop wood. ",
+			info: "Lua",
+			text: "The ComputerCraft mod for the videogame Minecraft introduced programmable computers and robots (called turtles) to the game. In 2013 I was very active on the official ComputerCraft forums <b>creating various programs with the mod</b>. The two most successful ones were the 'Client for OpenPeripheral's Glasses' and 'Ultimate Wood Chopper' programs, both of which <b>featured session persistence</b> by saving and loading their state. The programs were <b>downloaded over 12,000+ times</b> from pastebin. Creating a lot of these programs taught me a lot about <b>how to think in code</b>.<br/>The linked video was made by SeleckPlays on YT who approached me and asked if he was allowed to create a tutorial on the Ultimate Wood Chooper program I created. At the time this program was highly efficient when it came to producing wood and bio-fuel thanks to the Fir Wood tree not checking for any leaves being in the way and instantly grew which allowed the turtle to non-stop chop wood. ",
 			links: [
 				{
 					website: "github",
